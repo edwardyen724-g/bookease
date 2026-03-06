f48d40a54c0f33e9a5af1423ac46b1bd53747380
edwardyen724-g <65644998+edwardyen724-g@users.noreply.github.com>
2026-03-06 04:47:03 -0800

    fix(a9b): CI error fix attempt 4

--- src/context/SupabaseProvider.ts
+++ src/context/SupabaseProvider.ts
@@ -1,1 +1,11 @@
-import { SupabaseClient } from '@supabase/supabase-js'; import { createContext, ReactNode, useContext } from 'react'; const SupabaseContext = createContext<SupabaseClient | null>(null); export const SupabaseProvider = ({ children }: { children: ReactNode }) => { const supabase = new SupabaseClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!); return <SupabaseContext.Provider value={supabase}>{children}</SupabaseContext.Provider>; }; export const useSupabase = () => useContext(SupabaseContext);
+import { SupabaseClient } from '@supabase/supabase-js';
+import { createContext, ReactNode, useContext } from 'react';
+
+const SupabaseContext = createContext<SupabaseClient | null>(null);
+
+export const SupabaseProvider = ({ children }: { children: ReactNode }) => {
+  const supabase = new SupabaseClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+  return <SupabaseContext.Provider value={supabase}>{children}</SupabaseContext.Provider>;
+};
+
+export const useSupabase = () => useContext(SupabaseContext);
